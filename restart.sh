8b738a2fbbc111ce97a17e4cd7f23db925f44657
Patrick Deglon <pdeglon@gmail.com>
2025-12-01 22:27:47 -0800

    chore: release v0.1.4

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker compose -f /opt/n8n/docker-compose.yml up -d
+sudo docker compose -f /opt/n8n/docker-compose.yml restart n8n
+# sudo docker exec -it n8n sh -lc 'ls -la /home/node/.n8n/custom/node_modules/n8n-nodes-demeterics/dist/nodes'
 
